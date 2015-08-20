15d161825f8ca0d97e8bb90e23c121d42eff6b33
PChicken <tardisbluedoctorwho1@gmail.com>
2015-08-20 09:18:45 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,13 @@
-function ask
+minetest.register_on_joinplayer()
 minetest.register_chatcommand("gc", {
-  params = "<gchelp/m/d/s/a>",
-  description = "do gchelp as param for help.",
+  params = "<m/d/s/a/accept/decline>",
+  description = "\na = add player\accept = accept invite\ndecline = ",
   privs = {"gcmake"},
   func =  function(name, param)
-    
+    local params = minetest.splittext(param)
+    if params[1] = "a" then
+      global chataskees = chataskees + param[2]
+      minetest.chat_send_player(name, "Hello "..name"!\n"..asker.." wants you to join their chatgroup. To accept "..asker.."'s inivitation, type in:\n/gc accept\nTo decline, replace 'accept' with 'decline'.")
+      
   end,
 })
